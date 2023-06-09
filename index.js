289144e434860da4843b3bf39876326d606682db
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-10 02:34:46 +0600

    added my class route

--- index.js
+++ index.js
@@ -92,6 +92,23 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/classes/my-classes', verifyJWT, verifyInstructor, async (req, res) => {
+            const email = req.query.email;
+
+            if (!email) {
+                res.send([]);
+            }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+
+            const query = { instructorEmail: email };
+            const result = await classCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post('/classes', verifyJWT, verifyInstructor, async (req, res) => {
             const newClass = req.body;
             const result = await classCollection.insertOne(newClass);
