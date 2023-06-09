360309cb285e7966c190414c6127e15f34633003
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-09 12:54:41 +0600

    Added make instructor api

--- index.js
+++ index.js
@@ -59,6 +59,20 @@ async function run() {
 
         })
 
+        app.patch('/user/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+
+            const result = await userCollection.updateOne(query, updateDoc).toArray();
+            res.send(result);
+        })
+
         // get user
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email
