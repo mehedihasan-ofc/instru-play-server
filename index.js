a3913f5c0bfda03fce262b6849ed56189dd4e794
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-09 17:25:52 +0600

    Added JWT Token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,25 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    // bearer token
+    const token = authorization.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res
+                .status(401)
+                .send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 
 // ===================================================>
@@ -31,6 +51,13 @@ async function run() {
         const userCollection = client.db('instruPlayDB').collection('users');
         const classCollection = client.db('instruPlayDB').collection('classes');
         const cartCollection = client.db('instruPlayDB').collection('carts');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
 
         // class related apis
         app.get('/classes', async (req, res) => {
@@ -59,7 +86,21 @@ async function run() {
 
         })
 
-        app.patch('/user/instructor/:id', async (req, res) => {
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+
+            const result = await userCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
 
@@ -69,7 +110,7 @@ async function run() {
                 },
             };
 
-            const result = await userCollection.updateOne(query, updateDoc).toArray();
+            const result = await userCollection.updateOne(query, updateDoc);
             res.send(result);
         })
 
@@ -81,12 +122,6 @@ async function run() {
             res.send(result);
         })
 
-        // instructors
-        // app.get('/instructors', async (req, res => {
-        //     const query = {role: 'instructor'};
-        //     const result = await userCollection.find(query)
-        // }))
-
         app.get('/instructors', async (req, res) => {
             const query = { role: 'instructor' };
             const result = await userCollection.find(query).sort({ students: -1 }).toArray();
@@ -94,11 +129,16 @@ async function run() {
         })
 
         // get selected class by email
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
 
             if (!email) {
                 res.send([]);
+            }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
             }
 
             const query = { email: email };
